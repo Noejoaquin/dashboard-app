8f9cf3779f983c7b31c03e6ddb1fef5e0e6ed6b1
Noe <noe.joaquin.james@gmail.com>
2023-11-02 15:45:32 -0400

    fix images

--- app/page.tsx
+++ app/page.tsx
@@ -32,15 +32,15 @@ export default function Page() {
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className="sm:hidden md:block"
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
           />
-          <Image 
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Screenshots of dashboard project showing on mobile devices"
+            alt="Screenshot of the dashboard project showing mobile version"
           />
         </div>
       </div>
