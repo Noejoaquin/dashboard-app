0dd9849725286d67a01e45e86d0d8e7ad35fe78a
Noe <noe.joaquin.james@gmail.com>
2023-11-08 14:48:07 -0500

    set default

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,7 +4,15 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
 export default async function Page({ params }: { params: { id: string } } ) {
   const id = params.id;
-  const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+  let [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+  if (invoice === undefined) {
+    invoice = {
+      id: 'aaaaaa',
+      amount: 485,
+      status: 'pending',
+      customer_id: 'aaaa'
+    };
+  }
   return (
     <main>
       <Breadcrumbs
